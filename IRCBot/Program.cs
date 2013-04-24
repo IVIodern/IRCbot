eafd6cf874469b07dd9d7b50f44143c1363abf78
IVIodern <peterdoyle04@hotmail.com>
2013-04-24 17:45:49 +0100

    Initial import

---
+++ IRCBot/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Amrykid.Web.IRC;
+namespace IRCBot
+{
+    class Program
+    {
+        public static string server = "irc.rizon.net";
+        public static string channel = "#IIIIII";
+        public static string chanp = "iangotfiredfromarev";
+        public static string nick = "testingorz-";
+        public static int port = 6667;
+        public static IRC _irc;
+        static void Main(string[] args)
+        {
+            _irc = new IRC("Nothing");
+            _irc.Nick = nick;
+            _irc.Connect(server, port);
+            _irc.Logon(nick, nick);
+            _irc.Join(channel, chanp);
+            _irc.IRCMessageRecieved += new IRC.IRCMessageRecievedHandler(_irc_IRCMessageRecieved);
+            while (true)
+            {
+                _irc.ProcessEvents(0);
+            }
+        }
+
+        static void _irc_IRCMessageRecieved(object sender, string message, User user, IRCChannel chan)
+        {
+            if (message.StartsWith("!lol"))
+            {
+                _irc.SendMessage(  user.Nick + " is now laughing out loud!" + IRC.SupportedColors.NavyBlue, chan.Channel);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*static void _irc_IRCMessageRecieved(object sender, string message, User user, IRCChannel chan)
+{
+    if (message.StartsWith("!"))
+    {
+        if (message.Replace("!", "").StartsWith("time"))
+        {
+            _irc.SendMessage("Your current time is: " + DateTime.Now.ToLongTimeString(), IRC.SupportedColors.NavyBlue, chan.Channel);
+        }
+    }
+}*/
